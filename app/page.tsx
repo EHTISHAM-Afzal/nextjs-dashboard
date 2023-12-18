3f38e97b31929f17f01271628e2219e853bbb9f6
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-12-18 19:25:58 +0500

    modified:   app/lib/data.ts 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 import { Metadata } from 'next';
+import { lusitana } from './ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
