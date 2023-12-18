8a526516d31028a3d4b19668c39c511aca00142b
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-12-18 09:49:53 +0500

    Added login feature and metaData Changes to be committed: 	modified:   app/dashboard/customers/page.tsx 	modified:   app/dashboard/invoices/[id]/edit/page.tsx 	modified:   app/dashboard/invoices/create/page.tsx 	modified:   app/dashboard/invoices/page.tsx 	renamed:    public/favicon.ico -> app/favicon.ico 	modified:   app/layout.tsx 	modified:   app/lib/actions.ts 	modified:   app/login/page.tsx 	renamed:    public/opengraph-image.png -> app/opengraph-image.png 	modified:   app/page.tsx 	modified:   app/ui/dashboard/sidenav.tsx 	modified:   app/ui/login-form.tsx 	new file:   auth.config.ts 	new file:   auth.ts 	new file:   middleware.ts 	modified:   package-lock.json 	modified:   package.json

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
 const Customers = () => {
-  return (
-    <div>Customers Page</div>
-  )
-}
+  return <div>Customers Page</div>;
+};
 
-export default Customers
+export default Customers;
